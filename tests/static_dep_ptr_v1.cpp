0902f6d8b9058546b9a19d523053b10293eefd10
michaelyhuang23 <michaelyhuang23@gmail.com>
2022-04-18 23:38:34 -0400

    update parallel

--- tests/static_dep_ptr_v1.cpp
+++ tests/static_dep_ptr_v1.cpp
@@ -60,40 +60,40 @@ int main(int argc, char* argv[]) {
 
 	// std::cout<<"preprocessing time: "<<time.get_next()<<std::endl;
 
-	for(int i=backLoc[m-2]+1; i<n; i++){
-		depPtr[i] = -1;
-		root->activateItem(i);
-	}
-	// parlay::parallel_for(backLoc[m-2]+1, n, [&](int i){
+	// for(int i=backLoc[m-2]+1; i<n; i++){
 	// 	depPtr[i] = -1;
 	// 	root->activateItem(i);
-	// });
+	// }
+	parlay::parallel_for(backLoc[m-2]+1, n, [&](int i){
+		depPtr[i] = -1;
+		root->activateItem(i);
+	});
 	
 	for(int i=m-2;i>=0;i--){
 		int st = 0, et = backLoc[i];
 		if(i>0) st = backLoc[i-1]+1;
 
-		for(int i=st;i<et+1;i++){
-			pointD* ptr = root->NearestNeighbor(i);
-			if(ptr)
-				depPtr[i] = ptr->attribute;
-			else
-				depPtr[i] = -1;
-		}
-		// parlay::parallel_for(st, et+1, [&](int i){
+		// for(int i=st;i<et+1;i++){
 		// 	pointD* ptr = root->NearestNeighbor(i);
 		// 	if(ptr)
 		// 		depPtr[i] = ptr->attribute;
 		// 	else
 		// 		depPtr[i] = -1;
-		// });
+		// }
+		parlay::parallel_for(st, et+1, [&](int i){
+			pointD* ptr = root->NearestNeighbor(i);
+			if(ptr)
+				depPtr[i] = ptr->attribute;
+			else
+				depPtr[i] = -1;
+		});
 
-		for(int i=st;i<et+1;i++){
-			root->activateItem(i);
-		}
-		// parlay::parallel_for(st, et+1, [&](int i){
+		// for(int i=st;i<et+1;i++){
 		// 	root->activateItem(i);
-		// });
+		// }
+		parlay::parallel_for(st, et+1, [&](int i){
+			root->activateItem(i);
+		});
 	}
 
 	// std::cout<<"update query time: "<<time.get_next()<<std::endl;
