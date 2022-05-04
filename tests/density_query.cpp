358b15b051daa96534104dd9dbe46228a09f9409
Michael Huang <michaelyhuang23@gmail.com>
2022-05-03 22:03:54 -0400

    little change

--- tests/density_query.cpp
+++ tests/density_query.cpp
@@ -12,7 +12,7 @@
 #include "kdTree/kdTree.h"
 
 static const int dim = 2;
-static const double drange = 2;
+static const double drange = 20;
 using point = pargeo::point<dim>;
 using pointD = pargeo::pointD<dim, int>;
 
@@ -28,8 +28,9 @@ parlay::sequence<pointD> compute_densities(parlay::sequence<point>& ptrs){
 	
 
 	parlay::sequence<pointD> ptrDs(ptrs.size());
+
 	parlay::parallel_for(0, ptrs.size(), [&](size_t i){
-		return ptrDs[i] = pointD(ptrs[i].coords(), pargeo::kdTree::rangeCount(tree, ptrs[i], drange));
+		ptrDs[i] = pointD(ptrs[i].coords(), pargeo::kdTree::rangeCount(tree, ptrs[i], drange));
 	});
 
 	return ptrDs;
@@ -40,14 +41,20 @@ parlay::sequence<pointD> compute_densities(parlay::sequence<point>& ptrs){
 int main(int argc, char* argv[]) {
 	std::ios_base::sync_with_stdio(0);
 
+	pargeo::timer time;
+
 	pargeo::commandLine P(argc, argv, "<inFile>");
 	char* iFile = P.getArgument(0);
 
 	parlay::sequence<point> ptrs = pargeo::pointIO::readPointsFromFile<point>(iFile);
+
+	time.start();
 
 	parlay::sequence<pointD> ptrDs = compute_densities(ptrs);
 
 	for(int i=0;i<ptrDs.size();i++){
 		std::cout<<ptrDs[i][0]<<" "<<ptrDs[i][1]<<" "<<ptrDs[i].attribute<<'\n';
 	}
+
+	std::cout<<"density time: "<<time.get_next()<<std::endl;
 }
