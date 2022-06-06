094360357472d8b01951117bf0bda29ae17d369a
michaelyhuang23 <michaelyhuang23@gmail.com>
2022-06-06 01:02:43 -0400

    fenwickKdTree code complete and runnable but very slow

--- tests/dep_ptr_par.cpp
+++ tests/dep_ptr_par.cpp
@@ -10,55 +10,52 @@
 #include "pargeo/point.h"
 #include "pargeo/getTime.h"
 #include "pseudoDynamicKdTree/pdKdTree.h"
+#include "fenwickKdTree/fenwickKdTree.h"
 
 static const int dim = 2;
 using point = pargeo::point<dim>;
 using pointD = pargeo::pointD<dim, int>;
 
-
 int main(int argc, char* argv[]) {
 	std::ios_base::sync_with_stdio(0);
 
-	pargeo::timer queryT, updateT;
+	pargeo::timer prepT, queryT;
 
 	pargeo::commandLine P(argc, argv, "-i{inFile}");
 	char* iFile = P.getOptionValue("-i");
 
-	parlay::sequence<pointD> ptrs = pargeo::pointIO::readPointsFromFile<pointD>(iFile);
+	parlay::sequence<pointD> ptrsD = pargeo::pointIO::readPointsFromFile<pointD>(iFile);
 
-	int n = ptrs.size();
+	prepT.start();
+	int n = ptrsD.size();
+	
+	parlay::stable_sort_inplace(ptrsD, pointD::attCompRev);
+	parlay::sequence<point> ptrs(n);
+	parlay::parallel_for(0, n, [&](size_t i){ ptrs[i] = point(ptrsD[i].coords()); });
+		
+	pargeo::fenwickKdTree<dim> trees(ptrs);
+
 	parlay::sequence<int> depPtr(n);
-
-	parlay::stable_sort_inplace(ptrs, pointD::attCompRev);
-	parlay::sequence<pointD> sptrs(ptrs);
-
-	pargeo::pdKdTree::tree<dim, pointD>* root = pargeo::pdKdTree::build<dim, pointD>(sptrs, true, 16);
-	root->pargeo::pdKdTree::node<dim, pointD>::initParallel();
-
 	depPtr[0] = -1;
-	root->activateItem(0);
+
+	std::cout<<"preprocess time: "<<prepT.get_next()<<std::endl;
+
 	queryT.start();
-
-	
-	for(int i=1;i<n;i++){
-		pointD* ptr = root->NearestNeighbor(i);
-	
-		if(ptr)
-			depPtr[i] = ptr->attribute;
-		else
-			depPtr[i] = -1;
-
-		root->activateItem(i);
+	for(size_t idx = 1;idx<n;idx++){
+		depPtr[idx] = trees.query(idx-1, ptrs[idx]).first;
 	}
+//	parlay::parallel_for(1, n, [&](size_t idx){
+//								   depPtr[idx] = trees.query(idx-1, ptrs[idx]).first;
+//							   });
 
 	std::cout<<"query time: "<<queryT.get_next()<<std::endl;
+	std::cout<<"qc: "<<trees.qc<<std::endl;
+
 
 	for(int i=0;i<n;i++){
 	 	std::cout<<i<<"  ;  "<<ptrs[i][0]<<" "<<ptrs[i][1]<<":"<<ptrs[depPtr[i]][0]<<" "<<ptrs[depPtr[i]][1]<<"  ;  "<<depPtr[i]<<" ; "<<ptrs[i].dist(ptrs[depPtr[i]])<<std::endl;
 	}
-
 }
-
 
 
 
