2be4028e0147ee58b3d68d9c2bf347daad144b8d
michaelyhuang23 <michaelyhuang23@gmail.com>
2022-06-06 15:39:26 -0400

    return to functioning state

--- include/fenwickKdTree/fenwickKdTree.h
+++ include/fenwickKdTree/fenwickKdTree.h
@@ -11,36 +11,37 @@
 namespace pargeo {
 
 	template <int dim>
-		struct fenwickKdTree{
-			using point = pargeo::point<dim>;
-			using pointD = pargeo::pointD<dim, int>;
-			int n;
-			int qc;
-			parlay::sequence<point> ptrs;
-			parlay::sequence<pargeo::kdTree::node<dim, point>*> trees;
-			static inline size_t lowbit(size_t num){ return num&(-num); }
-			fenwickKdTree(parlay::sequence<point>& ptrs_) : n(ptrs_.size()), ptrs(ptrs_) {
-				qc = 0;
-				trees = parlay::sequence<pargeo::kdTree::node<dim, point>*>(n+1);
-				parlay::parallel_for(1, n+1, [&](size_t i){
-						size_t st = i-lowbit(i)+1-1;
-						trees[i] = pargeo::kdTree::build<dim, point>(ptrs.cut(st, i+1-1), true, 16);					});
-			}
+	struct fenwickKdTree{
+		using point = pargeo::point<dim>;
+		using pointD = pargeo::pointD<dim, int>;
+		int n;
+		int qc;
+		parlay::sequence<point> ptrs;
+		parlay::sequence<pargeo::kdTree::node<dim, point>*> trees;
+		static inline size_t lowbit(size_t num){ return num&(-num); }
+		fenwickKdTree(parlay::sequence<point>& ptrs_) : n(ptrs_.size()), ptrs(ptrs_) {
+			qc = 0;
+			trees = parlay::sequence<pargeo::kdTree::node<dim, point>*>(n+1);
+			parlay::parallel_for(1, n+1,
+								 [&](size_t i){
+									 size_t st = i-lowbit(i)+1-1;
+									 trees[i] = pargeo::kdTree::build<dim, point>(ptrs.cut(st, i+1-1), true, 16);});
+		}
 	
-			std::pair<size_t, point> query(int idx, point& q){
-				idx++;
-				double dist = std::numeric_limits<double>::max()/2;
-				point* op = NULL;
-				for(; idx > 0; idx -= lowbit(idx)){
-					point* p = pargeo::kdTree::NearestNeighbor<dim, point>(q, trees[idx]);
-					qc++;
-					if(p->dist(q) < dist){
-						dist = p->dist(q);
-						op = p;
-					}
+		std::pair<size_t, point> query(int idx, point& q){ // query range [0, idx] for item idx
+			idx++;
+			double dist = std::numeric_limits<double>::max()/2;
+			point* op = NULL;
+			for(; idx > 0; idx -= lowbit(idx)){
+				point* p = pargeo::kdTree::NearestNeighbor<dim, point>(q, trees[idx]);
+				qc++;
+				if(p->dist(q) < dist){
+					dist = p->dist(q);
+					op = p;
 				}
-				return std::make_pair(op-ptrs.data(), *op);
 			}
-		};
+			return std::make_pair(op-ptrs.data(), *op);
+		}
+	};
 
 };
