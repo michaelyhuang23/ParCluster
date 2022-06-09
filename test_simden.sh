8801affb34115caba5ff29d0c9e0c870bad4cee2
Michael Huang <michaelyhuang23@gmail.com>
2022-06-09 01:34:42 +0000

    new experiments

--- test_simden.sh
+++ test_simden.sh
@@ -1,36 +1,40 @@
-for((i=1000; i<=100000; i*=10)); do
+R=100000
+
+for((i=1000; i<=$R; i*=10)); do
     echo $i
     ./build/tests/seedspread_generator -s 0 -d 2 $i dataset/simden/${i}.txt
 done
 
+for((s=1; s<=60; s+=1)); do
+	echo ${s}
+	for((i=1000; i<=$R; i*=10)); do
+		r=10
+		echo $i
+		PARLAY_NUM_THREADS=${s} ./build/tests/density_query -r $r -i dataset/simden/${i}.txt -o dataset/simden/${i}_dense.txt > results/simden/density_${r}_${i}_${s}.txt
+		PARLAY_NUM_THREADS=${s} ./build/tests/dep_ptr_brute -i dataset/simden/${i}_dense.txt > results/simden/brute_${r}_${i}_${s}.txt
+		PARLAY_NUM_THREADS=${s} ./build/tests/dep_ptr_dyn -i dataset/simden/${i}_dense.txt > results/simden/dyn_${r}_${i}_${s}.txt
+		PARLAY_NUM_THREADS=${s} ./build/tests/dep_ptr_static -i dataset/simden/${i}_dense.txt > results/simden/static_${r}_${i}_${s}.txt
+		PARLAY_NUM_THREADS=${s} ./build/tests/dep_ptr_par -i dataset/simden/${i}_dense.txt > results/simden/par_${r}_${i}_${s}.txt
+	done
 
-for((i=1000; i<=100000; i*=10)); do
-    r=10
-    echo $i
-    ./build/tests/density_query -r $r -i dataset/simden/${i}.txt -o dataset/simden/${i}_dense.txt
-    ./build/tests/dep_ptr_brute -i dataset/simden/${i}_dense.txt > results/simden/brute_${r}_${i}.txt
-    ./build/tests/dep_ptr_dyn -i dataset/simden/${i}_dense.txt > results/simden/dyn_${r}_${i}.txt
-    ./build/tests/dep_ptr_static -i dataset/simden/${i}_dense.txt > results/simden/static_${r}_${i}.txt
-	./build/tests/dep_ptr_par -i dataset/simden/${i}_dense.txt > results/simden/par_${r}_${i}.txt
-done
+	for((i=1000; i<=$R; i*=10)); do
+		r=40
+		echo $i
+		PARLAY_NUM_THREADS=${s} ./build/tests/density_query -r $r -i dataset/simden/${i}.txt -o dataset/simden/${i}_dense.txt > results/simden/density_${r}_${i}_${s}.txt
+		PARLAY_NUM_THREADS=${s} ./build/tests/dep_ptr_brute -i dataset/simden/${i}_dense.txt > results/simden/brute_${r}_${i}_${s}.txt
+		PARLAY_NUM_THREADS=${s} ./build/tests/dep_ptr_dyn -i dataset/simden/${i}_dense.txt > results/simden/dyn_${r}_${i}_${s}.txt
+		PARLAY_NUM_THREADS=${s} ./build/tests/dep_ptr_static -i dataset/simden/${i}_dense.txt > results/simden/static_${r}_${i}_${s}.txt
+		PARLAY_NUM_THREADS=${s} ./build/tests/dep_ptr_par -i dataset/simden/${i}_dense.txt > results/simden/par_${r}_${i}_${s}.txt
+	done
 
-for((i=1000; i<=100000; i*=10)); do
-    r=40
-    echo $i
-    ./build/tests/density_query -r $r -i dataset/simden/${i}.txt -o dataset/simden/${i}_dense.txt
-    ./build/tests/dep_ptr_brute -i dataset/simden/${i}_dense.txt > results/simden/brute_${r}_${i}.txt
-    ./build/tests/dep_ptr_dyn -i dataset/simden/${i}_dense.txt > results/simden/dyn_${r}_${i}.txt
-    ./build/tests/dep_ptr_static -i dataset/simden/${i}_dense.txt > results/simden/static_${r}_${i}.txt
-	./build/tests/dep_ptr_par -i dataset/simden/${i}_dense.txt > results/simden/par_${r}_${i}.txt
-done
-
-for((i=1000; i<=100000; i*=10)); do
-    r=160
-    echo $i
-    ./build/tests/density_query -r $r -i dataset/simden/${i}.txt -o dataset/simden/${i}_dense.txt
-    ./build/tests/dep_ptr_brute -i dataset/simden/${i}_dense.txt > results/simden/brute_${r}_${i}.txt
-    ./build/tests/dep_ptr_dyn -i dataset/simden/${i}_dense.txt > results/simden/dyn_${r}_${i}.txt
-    ./build/tests/dep_ptr_static -i dataset/simden/${i}_dense.txt > results/simden/static_${r}_${i}.txt
-	./build/tests/dep_ptr_par -i dataset/simden/${i}_dense.txt > results/simden/par_${r}_${i}.txt
+	for((i=1000; i<=$R; i*=10)); do
+		r=160
+		echo $i
+		PARLAY_NUM_THREADS=${s} ./build/tests/density_query -r $r -i dataset/simden/${i}.txt -o dataset/simden/${i}_dense.txt > results/simden/density_${r}_${i}_${s}.txt
+		PARLAY_NUM_THREADS=${s} ./build/tests/dep_ptr_brute -i dataset/simden/${i}_dense.txt > results/simden/brute_${r}_${i}_${s}.txt
+		PARLAY_NUM_THREADS=${s} ./build/tests/dep_ptr_dyn -i dataset/simden/${i}_dense.txt > results/simden/dyn_${r}_${i}_${s}.txt
+		PARLAY_NUM_THREADS=${s} ./build/tests/dep_ptr_static -i dataset/simden/${i}_dense.txt > results/simden/static_${r}_${i}_${s}.txt
+		PARLAY_NUM_THREADS=${s} ./build/tests/dep_ptr_par -i dataset/simden/${i}_dense.txt > results/simden/par_${r}_${i}_${s}.txt
+	done
 done
 
--- test_simden_fast.sh
+++ test_simden_fast.sh
@@ -6,27 +6,29 @@ for((i=1000; i<=$R; i*=10)); do
 done
 
 
-for((i=1000; i<=$R; i*=10)); do
-    r=10
-    echo $i
-    ./build/tests/density_query -r $r -i dataset/simden/${i}.txt -o dataset/simden/${i}_dense.txt
-    ./build/tests/dep_ptr_static -i dataset/simden/${i}_dense.txt > results/simden/static_${r}_${i}.txt
-	./build/tests/dep_ptr_par -i dataset/simden/${i}_dense.txt > results/simden/par_${r}_${i}.txt
-done
+for((s=1; s<=60; s+=1)); do
+	echo ${s}
+	for((i=1000; i<=$R; i*=10)); do
+		r=10
+		echo $i
+		PARLAY_NUM_THREADS=${s} ./build/tests/density_query -r $r -i dataset/simden/${i}.txt -o dataset/simden/${i}_dense.txt > results/simden/density_${r}_${i}_${s}.txt
+		PARLAY_NUM_THREADS=${s} ./build/tests/dep_ptr_static -i dataset/simden/${i}_dense.txt > results/simden/static_${r}_${i}_${s}.txt
+		PARLAY_NUM_THREADS=${s} ./build/tests/dep_ptr_par -i dataset/simden/${i}_dense.txt > results/simden/par_${r}_${i}_${s}.txt
+	done
 
-for((i=1000; i<=$R; i*=10)); do
-    r=40
-    echo $i
-    ./build/tests/density_query -r $r -i dataset/simden/${i}.txt -o dataset/simden/${i}_dense.txt
-    ./build/tests/dep_ptr_static -i dataset/simden/${i}_dense.txt > results/simden/static_${r}_${i}.txt
-	./build/tests/dep_ptr_par -i dataset/simden/${i}_dense.txt > results/simden/par_${r}_${i}.txt
-done
+	for((i=1000; i<=$R; i*=10)); do
+		r=40
+		echo $i
+		PARLAY_NUM_THREADS=${s} ./build/tests/density_query -r $r -i dataset/simden/${i}.txt -o dataset/simden/${i}_dense.txt > results/simden/density_${r}_${i}_${s}.txt
+		PARLAY_NUM_THREADS=${s} ./build/tests/dep_ptr_static -i dataset/simden/${i}_dense.txt > results/simden/static_${r}_${i}_${s}.txt
+		PARLAY_NUM_THREADS=${s} ./build/tests/dep_ptr_par -i dataset/simden/${i}_dense.txt > results/simden/par_${r}_${i}_${s}.txt
+	done
 
-for((i=1000; i<=$R; i*=10)); do
-    r=160
-    echo $i
-    ./build/tests/density_query -r $r -i dataset/simden/${i}.txt -o dataset/simden/${i}_dense.txt
-    ./build/tests/dep_ptr_static -i dataset/simden/${i}_dense.txt > results/simden/static_${r}_${i}.txt
-	./build/tests/dep_ptr_par -i dataset/simden/${i}_dense.txt > results/simden/par_${r}_${i}.txt
+	for((i=1000; i<=$R; i*=10)); do
+		r=160
+		echo $i
+		PARLAY_NUM_THREADS=${s} ./build/tests/density_query -r $r -i dataset/simden/${i}.txt -o dataset/simden/${i}_dense.txt > results/simden/density_${r}_${i}_${s}.txt
+		PARLAY_NUM_THREADS=${s} ./build/tests/dep_ptr_static -i dataset/simden/${i}_dense.txt > results/simden/static_${r}_${i}_${s}.txt
+		PARLAY_NUM_THREADS=${s} ./build/tests/dep_ptr_par -i dataset/simden/${i}_dense.txt > results/simden/par_${r}_${i}_${s}.txt
+	done
 done
-
