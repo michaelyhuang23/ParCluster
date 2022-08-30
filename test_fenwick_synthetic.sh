bcf0f8e94ed317ff2a184239400c6fcce4b8c5c6
Michael Huang <michaelyhuang23@gmail.com>
2022-08-30 21:27:36 +0000

    change r to 100

--- test_fenwick_synthetic.sh
+++ test_fenwick_synthetic.sh
@@ -10,7 +10,7 @@ done
 for((s=30; s<=60; s*=2)); do
 	echo ${s}
 	for((i=1000; i<=$R; i*=10)); do
-		r=10
+		r=100
 		echo $i
 		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n 0 -d 20 -i dataset/simden/${i}.txt > results/simden1/fenwick_${r}_${i}_${s}.txt
 		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n 0 -d 20 -i dataset/varden/${i}.txt > results/varden1/fenwick_${r}_${i}_${s}.txt
@@ -21,7 +21,7 @@ done
 for((s=1; s<=16; s*=2)); do
 	echo ${s}
 	for((i=1000; i<=$R; i*=10)); do
-		r=10
+		r=100
 		echo $i
 		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n 0 -d 20 -i dataset/simden/${i}.txt > results/simden1/fenwick_${r}_${i}_${s}.txt
 		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n 0 -d 20 -i dataset/varden/${i}.txt > results/varden1/fenwick_${r}_${i}_${s}.txt
@@ -36,7 +36,7 @@ done
 for((s=30; s<=60; s*=2)); do
 	echo ${s}
 	for((i=1000; i<=$R; i*=10)); do
-		r=10
+		r=100
 		echo $i
 		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n 0 -d 20 -i dataset/simden/${i}.txt > results/simden2/fenwick_${r}_${i}_${s}.txt
 		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n 0 -d 20 -i dataset/varden/${i}.txt > results/varden2/fenwick_${r}_${i}_${s}.txt
@@ -47,7 +47,7 @@ done
 for((s=1; s<=16; s*=2)); do
 	echo ${s}
 	for((i=1000; i<=$R; i*=10)); do
-		r=10
+		r=100
 		echo $i
 		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n 0 -d 20 -i dataset/simden/${i}.txt > results/simden2/fenwick_${r}_${i}_${s}.txt
 		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n 0 -d 20 -i dataset/varden/${i}.txt > results/varden2/fenwick_${r}_${i}_${s}.txt
@@ -61,7 +61,7 @@ done
 for((s=30; s<=60; s*=2)); do
 	echo ${s}
 	for((i=1000; i<=$R; i*=10)); do
-		r=10
+		r=100
 		echo $i
 		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n 0 -d 20 -i dataset/simden/${i}.txt > results/simden3/fenwick_${r}_${i}_${s}.txt
 		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n 0 -d 20 -i dataset/varden/${i}.txt > results/varden3/fenwick_${r}_${i}_${s}.txt
@@ -72,7 +72,7 @@ done
 for((s=1; s<=16; s*=2)); do
 	echo ${s}
 	for((i=1000; i<=$R; i*=10)); do
-		r=10
+		r=100
 		echo $i
 		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n 0 -d 20 -i dataset/simden/${i}.txt > results/simden3/fenwick_${r}_${i}_${s}.txt
 		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n 0 -d 20 -i dataset/varden/${i}.txt > results/varden3/fenwick_${r}_${i}_${s}.txt
