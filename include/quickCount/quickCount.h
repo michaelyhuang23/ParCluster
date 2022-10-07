d736511bf64b2b3707a9fa5f3e2944808567ba4b
Michael Huang <michaelyhuang23@gmail.com>
2022-10-06 20:33:31 -0400

    quickCount main body finished

--- include/quickCount/quickCount.h
+++ include/quickCount/quickCount.h
@@ -101,7 +101,65 @@ namespace pargeo {
 			return std::make_pair(std::move(Out), m);
 		}
 
-		void recurseCount(parlay::slice<pointT *, pointT *> points, paylay::slice<ballT *, ballT *> regions, pointT& pMin, pointT& pMax){
+		template <typename In_Seq, typename Type_Seq>
+		auto split_three(In_Seq const &In,
+					   Type_Seq const &Fl,
+					   parlay::flags fl = parlay::no_flag)
+			-> std::tuple<parlay::sequence<typename In_Seq::value_type>, size_t, size_t>
+		{
+
+			using namespace parlay;
+			using namespace parlay::internal;
+
+			using T = typename In_Seq::value_type;
+			size_t n = In.size();
+			size_t l = num_blocks(n, _block_size);
+			sequence<size_t> SumsC(l), SumsC2(l);
+			sliced_for(
+				n, _block_size,
+				[&](size_t i, size_t s, size_t e)
+					{
+						size_t c = 0, c1 = 0;
+						for (size_t j = s; j < e; j++){
+							c += (Fl[j] == 0);
+							c1 += (Fl[j] == 0 || Fl[j] == 1); // is left or mid
+						}
+						SumsC[i] = c;
+						SumsC1[i] = c1;
+					},
+				fl);
+			size_t m = scan_inplace(make_slice(SumsC), addm<size_t>());
+			size_t m1 = scan_inplace(make_slice(SumsC1), addm<size_t>());
+			sequence<T> Out = sequence<T>::uninitialized(n);
+			sliced_for(
+				n, _block_size,
+				[&](size_t i, size_t s, size_t e)
+					{
+						size_t c0 = SumsC[i];
+						size_t c1 = SumsC1[i] - SumsC[i] + m;
+						size_t c2 = s - SumC1[i] + m1;
+						for (size_t j = s; j < e; j++)
+						{
+							if (Fl[j] == 0)
+								assign_uninitialized(Out[c0++], In[j]);
+							else if (Fl[j] == 1)
+								assign_uninitialized(Out[c1++], In[j]);
+							else 
+								assign_uninitialized(Out[c2++], In[j]);
+						}
+					},
+				fl);
+			return std::make_tuple(std::move(Out), m, m1);
+		}
+
+		void recurseCount(parlay::slice<pointT *, pointT *> points, paylay::slice<ballT *, ballT *> regions, pointT& pMin, pointT& pMax, intT cutoff=16){
+
+			if(regions.size() * points.size() < cutoff){
+				for(intT i=0;i<regions.size();++i)
+					for(intT j=0;j<points.size();++j)
+						if(regions[i].contains_point(points[j])) regions[i].count++;
+			}
+
 			intT k = findWidest();
 			floatT xM = (pMax[k] + pMin[k]) / 2;
 			// Split points by xM in dim k (parallel)
@@ -109,18 +167,17 @@ namespace pargeo {
 			parlay::parallel_for(0, points.size(),
 								 [&](intT i)
 									 {
-										 if (points[i]->at(k) < xM)
+					ballT					 if (points[i]->at(k) < xM)
 											 flags[i] = 1;
 										 else
 											 flags[i] = 0;
 									 });
-			auto mySplit = kdTreeInternal::split_two(points, flags);
-			auto splited = mySplit.first;
-			intT median = mySplit.second;
-			parlay::parallel_for(0, points.size(), [&](intT i)
-												{ points[i] = splited[i]; }); // Copy back
+			auto pointSplit = split_two(points, flags);
+			sequence<pointT *> splitedPoints = pointSplit.first;
+			intT median = pointSplit.second; // no need to copy back
 
-			if (median == 0 || median == points.size())
+
+			if (median == 0 || median == points.size()) // consider getting rid of this
 			{
 				median = (points.size() / 2.0);
 			}
@@ -130,34 +187,33 @@ namespace pargeo {
 			boundingBoxParallel(points.cut(median, points.size()), pMinR, pMaxR);
 
 			// Now split the regions
-			sequence<bool> region_flags(regions.size());
+			sequence<intT> region_flags(regions.size());
 			parlay::parallel_for(0, regions.size(), [&](intT i){
-				if(regions[i]->at(k) < xM){
-					if(regions[i]->intersect(pMinR, pMaxR))
-						region_flags[i] = 2;
+				if(flags[i] == 1){ // to the left
+					if(regions[i]->intersect_rect(pMinR, pMaxR))
+						region_flags[i] = 1;
 					else
 						region_flags[i] = 0;
 				}else{
-					if(regions[i]->intersect(pMinL, pMaxL))
-						region_flags[i] = 2;
-					else
+					if(regions[i]->intersect_rect(pMinL, pMaxL))
 						region_flags[i] = 1;
+					else
+						region_flags[i] = 2;
 				}
 			});
-
-
-
+			auto regionSplit = split_three(regions, region_flags);
+			sequence<ballT *> splitedRegions = get<0>(regionSplit);
+			intT region_median = get<1>(regionSplit);
+			intT region_median1 = get<2>(regionSplit);
 
 			// Recursive construction
 			parlay::par_do(
-				[&]()
-					{ space[0] = nodeT(points.cut(0, median), median, space + 1, flags.cut(0, median), leafSize); },
-				[&]()
-					{ space[2 * median - 1] = nodeT(points.cut(median, size()), size() - median, space + 2 * median, flags.cut(median, size()), leafSize); });
-			left = space;
-			right = space + 2 * median - 1;
-			left->sib = right;
-			right->sib = left;
+				[&](){ 
+					recurseCount(splitedPoints.cut(0, median), splitedRegions.cut(0, region_median1), pMinL, pMaxL, cutoff);
+				},
+				[&](){
+					recurseCount(splitedPoints.cut(median, splitedPoints.size()), splitedRegions.cut(region_median, splitedRegions.size()), pMinR, pMaxR, cutoff);
+				});
 		}
 	};
 
