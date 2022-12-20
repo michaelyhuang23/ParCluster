46843a8d1e16cd74e1a8e74005b24e5a8711b6bb
Michael Huang <michaelyhuang23@gmail.com>
2022-12-19 22:16:31 -0500

    new correct query_local data

--- test_query.sh
+++ test_query.sh
@@ -6,16 +6,16 @@ n=0
 for((t=1; t<=3; t+=1)); do
     for((s=30; s<=60; s*=2)); do
 	echo ${s}
-	PARLAY_NUM_THREADS=${s} ./build/tests/dpc_priority -r $r -n $n -d $d -i dataset/reallife/query.pbbs > results/query$t/priority_${n}_${r}_${s}.txt
-	PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n $n -d $d -i dataset/reallife/query.pbbs > results/query$t/fenwick_${n}_${r}_${s}.txt
-	PARLAY_NUM_THREADS=${s} ./build/tests/dpc_static -r $r -n $n -d $d -i dataset/reallife/query.pbbs > results/query$t/static_${n}_${r}_${s}.txt
+	PARLAY_NUM_THREADS=${s} ./build/tests/dpc_priority -r $r -n $n -d $d -i dataset/reallife/query.pbbs > results/query_local$t/priority_${n}_${r}_${s}.txt
+	PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n $n -d $d -i dataset/reallife/query.pbbs > results/query_local$t/fenwick_${n}_${r}_${s}.txt
+	PARLAY_NUM_THREADS=${s} ./build/tests/dpc_static -r $r -n $n -d $d -i dataset/reallife/query.pbbs > results/query_local$t/static_${n}_${r}_${s}.txt
     done
 
     for((s=1; s<=16; s*=2)); do
 	echo ${s}
-	PARLAY_NUM_THREADS=${s} ./build/tests/dpc_priority -r $r -n $n -d $d -i dataset/reallife/query.pbbs > results/query$t/priority_${n}_${r}_${s}.txt
-	PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n $n -d $d -i dataset/reallife/query.pbbs > results/query$t/fenwick_${n}_${r}_${s}.txt
-	PARLAY_NUM_THREADS=${s} ./build/tests/dpc_static -r $r -n $n -d $d -i dataset/reallife/query.pbbs > results/query$t/static_${n}_${r}_${s}.txt
+	PARLAY_NUM_THREADS=${s} ./build/tests/dpc_priority -r $r -n $n -d $d -i dataset/reallife/query.pbbs > results/query_local$t/priority_${n}_${r}_${s}.txt
+	PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n $n -d $d -i dataset/reallife/query.pbbs > results/query_local$t/fenwick_${n}_${r}_${s}.txt
+	PARLAY_NUM_THREADS=${s} ./build/tests/dpc_static -r $r -n $n -d $d -i dataset/reallife/query.pbbs > results/query_local$t/static_${n}_${r}_${s}.txt
     done
 done
 
