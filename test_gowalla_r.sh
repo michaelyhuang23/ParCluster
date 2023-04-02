cf768ac2d6b855b7d58be3967edbd94f2e800dc9
Michael Huang <michaelyhuang23@gmail.com>
2023-04-02 02:36:02 +0000

    some results

--- test_gowalla_r.sh
+++ test_gowalla_r.sh
@@ -1,11 +1,11 @@
 # r=0.03
 d=40
 n=0
-s=60
+s=176
 
 
 for((t=1; t<=3; t+=1)); do
-    for((r=0.4; r<=50; r*=2)); do
+    for r in {0.4,0.8,1.6,3.2,6.4,12.8,25.6}; do
 	echo ${r}
 	PARLAY_NUM_THREADS=${s} ./build/tests/dpc_priority -r $r -n $n -d $d -i dataset/reallife/gowalla.pbbs > results/gowalla$t/priority_${n}_${r}_${s}.txt
 	PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n $n -d $d -i dataset/reallife/gowalla.pbbs > results/gowalla$t/fenwick_${n}_${r}_${s}.txt
--- test_sensor5_r.sh
+++ test_sensor5_r.sh
@@ -1,11 +1,11 @@
 # r=0.2
 d=2
 n=5
-s=60
+s=176
 
 
 for((t=1; t<=3; t+=1)); do
-    for((r=0.003; r<=1.5; r*=2)); do
+    for r in {0.003,0.006,0.012,0.024,0.048,0.096,0.192,0.384,0.768}; do
 	echo ${r}
 	PARLAY_NUM_THREADS=${s} ./build/tests/dpc_priority -r $r -n $n -d $d -i dataset/reallife/sensor5.dat > results/sensor5$t/priority_${n}_${r}_${s}.txt
 	PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n $n -d $d -i dataset/reallife/sensor5.dat > results/sensor5$t/fenwick_${n}_${r}_${s}.txt
