f4692d2ddb4f26688c6c587ea1a358160adbed98
Michael Huang <michaelyhuang23@gmail.com>
2023-04-03 00:38:12 +0000

    new results

--- test_fenwick_synthetic.sh
+++ test_fenwick_synthetic.sh
@@ -1,83 +1,26 @@
+s=60
 R=10000000
-r=30
 d=100
-
 for((i=1000; i<=$R; i*=10)); do
     echo $i
     ./build/tests/seedspread_generator -s 0 -d 2 $i dataset/simden/${i}.txt
-	./build/tests/seedspread_generator -s 1 -d 2 $i dataset/varden/${i}.txt
-	./build/tests/uniform_generator -d 2 $i dataset/uniform/${i}.txt
+    ./build/tests/seedspread_generator -s 1 -d 2 $i dataset/varden/${i}.txt
+    ./build/tests/uniform_generator -d 2 $i dataset/uniform/${i}.txt
 done
 
-for((s=30; s<=60; s*=2)); do
-	echo ${s}
+for t in {1,2,3}; do
+for r in {2,3,4.5,6.75,10.125,15.1875}; do
+	echo ${r}
 	for((i=1000; i<=$R; i*=10)); do
-		
 		echo $i
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n 0 -d $d -i dataset/simden/${i}.txt > results/simden1/fenwick_${r}_${i}_${s}.txt
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n 0 -d $d -i dataset/varden/${i}.txt > results/varden1/fenwick_${r}_${i}_${s}.txt
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n 0 -d $d -i dataset/uniform/${i}.txt > results/uniform1/fenwick_${r}_${i}_${s}.txt
+		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n 0 -d $d -i dataset/simden/${i}.txt > results/simden${t}/fenwick_${r}_${i}_${s}.txt
+		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n 0 -d $d -i dataset/varden/${i}.txt > results/varden${t}/fenwick_${r}_${i}_${s}.txt
+		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n 0 -d $d -i dataset/uniform/${i}.txt > results/uniform${t}/fenwick_${r}_${i}_${s}.txt
+		r/=3.16
 	done
 done
-
-for((s=1; s<=16; s*=2)); do
-	echo ${s}
-	for((i=1000; i<=$R; i*=10)); do
-		
-		echo $i
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n 0 -d $d -i dataset/simden/${i}.txt > results/simden1/fenwick_${r}_${i}_${s}.txt
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n 0 -d $d -i dataset/varden/${i}.txt > results/varden1/fenwick_${r}_${i}_${s}.txt
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n 0 -d $d -i dataset/uniform/${i}.txt > results/uniform1/fenwick_${r}_${i}_${s}.txt
-	done
 done
 
 
 
 
-
-for((s=30; s<=60; s*=2)); do
-	echo ${s}
-	for((i=1000; i<=$R; i*=10)); do
-		
-		echo $i
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n 0 -d $d -i dataset/simden/${i}.txt > results/simden2/fenwick_${r}_${i}_${s}.txt
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n 0 -d $d -i dataset/varden/${i}.txt > results/varden2/fenwick_${r}_${i}_${s}.txt
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n 0 -d $d -i dataset/uniform/${i}.txt > results/uniform2/fenwick_${r}_${i}_${s}.txt
-	done
-done
-
-for((s=1; s<=16; s*=2)); do
-	echo ${s}
-	for((i=1000; i<=$R; i*=10)); do
-		
-		echo $i
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n 0 -d $d -i dataset/simden/${i}.txt > results/simden2/fenwick_${r}_${i}_${s}.txt
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n 0 -d $d -i dataset/varden/${i}.txt > results/varden2/fenwick_${r}_${i}_${s}.txt
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n 0 -d $d -i dataset/uniform/${i}.txt > results/uniform2/fenwick_${r}_${i}_${s}.txt
-	done
-done
-
-
-
-
-for((s=30; s<=60; s*=2)); do
-	echo ${s}
-	for((i=1000; i<=$R; i*=10)); do
-		
-		echo $i
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n 0 -d $d -i dataset/simden/${i}.txt > results/simden3/fenwick_${r}_${i}_${s}.txt
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n 0 -d $d -i dataset/varden/${i}.txt > results/varden3/fenwick_${r}_${i}_${s}.txt
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n 0 -d $d -i dataset/uniform/${i}.txt > results/uniform3/fenwick_${r}_${i}_${s}.txt
-	done
-done
-
-for((s=1; s<=16; s*=2)); do
-	echo ${s}
-	for((i=1000; i<=$R; i*=10)); do
-		
-		echo $i
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n 0 -d $d -i dataset/simden/${i}.txt > results/simden3/fenwick_${r}_${i}_${s}.txt
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n 0 -d $d -i dataset/varden/${i}.txt > results/varden3/fenwick_${r}_${i}_${s}.txt
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n 0 -d $d -i dataset/uniform/${i}.txt > results/uniform3/fenwick_${r}_${i}_${s}.txt
-	done
-done
--- test_priority_synthetic.sh
+++ test_priority_synthetic.sh
@@ -1,5 +1,5 @@
+s=60
 R=10000000
-r=30
 d=100
 for((i=1000; i<=$R; i*=10)); do
     echo $i
@@ -8,75 +8,19 @@ for((i=1000; i<=$R; i*=10)); do
     ./build/tests/uniform_generator -d 2 $i dataset/uniform/${i}.txt
 done
 
-for((s=30; s<=60; s*=2)); do
-	echo ${s}
+for t in {1,2,3}; do
+for r in {2,3,4.5,6.75,10.125,15.1875}; do
+	echo ${r}
 	for((i=1000; i<=$R; i*=10)); do
-		
 		echo $i
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_priority -r $r -n 0 -d $d -i dataset/simden/${i}.txt > results/simden1/priority_${r}_${i}_${s}.txt
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_priority -r $r -n 0 -d $d -i dataset/varden/${i}.txt > results/varden1/priority_${r}_${i}_${s}.txt
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_priority -r $r -n 0 -d $d -i dataset/uniform/${i}.txt > results/uniform1/priority_${r}_${i}_${s}.txt
+		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_priority -r $r -n 0 -d $d -i dataset/simden/${i}.txt > results/simden${t}/priority_${r}_${i}_${s}.txt
+		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_priority -r $r -n 0 -d $d -i dataset/varden/${i}.txt > results/varden${t}/priority_${r}_${i}_${s}.txt
+		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_priority -r $r -n 0 -d $d -i dataset/uniform/${i}.txt > results/uniform${t}/priority_${r}_${i}_${s}.txt
+		r/=3.16
 	done
 done
-
-for((s=1; s<=16; s*=2)); do
-	echo ${s}
-	for((i=1000; i<=$R; i*=10)); do
-		
-		echo $i
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_priority -r $r -n 0 -d $d -i dataset/simden/${i}.txt > results/simden1/priority_${r}_${i}_${s}.txt
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_priority -r $r -n 0 -d $d -i dataset/varden/${i}.txt > results/varden1/priority_${r}_${i}_${s}.txt
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_priority -r $r -n 0 -d $d -i dataset/uniform/${i}.txt > results/uniform1/priority_${r}_${i}_${s}.txt
-	done
 done
 
 
 
 
-
-for((s=30; s<=60; s*=2)); do
-	echo ${s}
-	for((i=1000; i<=$R; i*=10)); do
-		
-		echo $i
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_priority -r $r -n 0 -d $d -i dataset/simden/${i}.txt > results/simden2/priority_${r}_${i}_${s}.txt
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_priority -r $r -n 0 -d $d -i dataset/varden/${i}.txt > results/varden2/priority_${r}_${i}_${s}.txt
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_priority -r $r -n 0 -d $d -i dataset/uniform/${i}.txt > results/uniform2/priority_${r}_${i}_${s}.txt
-	done
-done
-
-for((s=1; s<=16; s*=2)); do
-	echo ${s}
-	for((i=1000; i<=$R; i*=10)); do
-		
-		echo $i
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_priority -r $r -n 0 -d $d -i dataset/simden/${i}.txt > results/simden2/priority_${r}_${i}_${s}.txt
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_priority -r $r -n 0 -d $d -i dataset/varden/${i}.txt > results/varden2/priority_${r}_${i}_${s}.txt
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_priority -r $r -n 0 -d $d -i dataset/uniform/${i}.txt > results/uniform2/priority_${r}_${i}_${s}.txt
-	done
-done
-
-
-
-
-for((s=30; s<=60; s*=2)); do
-	echo ${s}
-	for((i=1000; i<=$R; i*=10)); do
-		
-		echo $i
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_priority -r $r -n 0 -d $d -i dataset/simden/${i}.txt > results/simden3/priority_${r}_${i}_${s}.txt
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_priority -r $r -n 0 -d $d -i dataset/varden/${i}.txt > results/varden3/priority_${r}_${i}_${s}.txt
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_priority -r $r -n 0 -d $d -i dataset/uniform/${i}.txt > results/uniform3/priority_${r}_${i}_${s}.txt
-	done
-done
-
-for((s=1; s<=16; s*=2)); do
-	echo ${s}
-	for((i=1000; i<=$R; i*=10)); do
-		
-		echo $i
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_priority -r $r -n 0 -d $d -i dataset/simden/${i}.txt > results/simden3/priority_${r}_${i}_${s}.txt
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_priority -r $r -n 0 -d $d -i dataset/varden/${i}.txt > results/varden3/priority_${r}_${i}_${s}.txt
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_priority -r $r -n 0 -d $d -i dataset/uniform/${i}.txt > results/uniform3/priority_${r}_${i}_${s}.txt
-	done
-done
