f82d407f99ec812dd535d3449064396a8577cd90
Michael Huang <michaelyhuang23@gmail.com>
2023-04-03 08:08:02 +0000

    fixed r

--- test_priority_synthetic.sh
+++ test_priority_synthetic.sh
@@ -12,11 +12,12 @@ for t in {1,2,3}; do
 for r in {2,3,4.5,6.75,10.125,15.1875}; do
 	echo ${r}
 	for((i=1000; i<=$R; i*=10)); do
-		echo $i
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_priority -r $r -n 0 -d $d -i dataset/simden/${i}.txt > results/simden${t}/priority_${r}_${i}_${s}.txt
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_priority -r $r -n 0 -d $d -i dataset/varden/${i}.txt > results/varden${t}/priority_${r}_${i}_${s}.txt
-		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_priority -r $r -n 0 -d $d -i dataset/uniform/${i}.txt > results/uniform${t}/priority_${r}_${i}_${s}.txt
-		r*=3.16
+	    echo $i
+	    echo $r
+	    PARLAY_NUM_THREADS=${s} ./build/tests/dpc_priority -r $r -n 0 -d $d -i dataset/simden/${i}.txt > results/simden${t}/priority_${r}_${i}_${s}.txt
+	    PARLAY_NUM_THREADS=${s} ./build/tests/dpc_priority -r $r -n 0 -d $d -i dataset/varden/${i}.txt > results/varden${t}/priority_${r}_${i}_${s}.txt
+	    PARLAY_NUM_THREADS=${s} ./build/tests/dpc_priority -r $r -n 0 -d $d -i dataset/uniform/${i}.txt > results/uniform${t}/priority_${r}_${i}_${s}.txt
+	    r=$(echo 3.16*$r | bc)
 	done
 done
 done
