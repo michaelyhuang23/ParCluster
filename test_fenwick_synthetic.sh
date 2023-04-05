a001fec25287290360d870fee844ce891c766891
Michael Huang <michaelyhuang23@gmail.com>
2023-04-04 22:47:48 -0400

    update random compute code

--- test_fenwick_synthetic.sh
+++ test_fenwick_synthetic.sh
@@ -9,14 +9,14 @@ for((i=1000; i<=$R; i*=10)); do
 done
 
 for t in {1,2,3}; do
-for r in {2,3,4.5,6.75,10.125,15.1875}; do
+for r in {2,3,4.5,6.75,10.125,15.1875,22.78125,34.171875,51.2578125}; do
 	echo ${r}
 	for((i=1000; i<=$R; i*=10)); do
 		echo $i
 		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n 0 -d $d -i dataset/simden/${i}.txt > results/simden${t}/fenwick_${r}_${i}_${s}.txt
 		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n 0 -d $d -i dataset/varden/${i}.txt > results/varden${t}/fenwick_${r}_${i}_${s}.txt
 		PARLAY_NUM_THREADS=${s} ./build/tests/dpc_fenwick -r $r -n 0 -d $d -i dataset/uniform/${i}.txt > results/uniform${t}/fenwick_${r}_${i}_${s}.txt
-		r/=3.16
+		r=$(echo 3.16*$r | bc)
 	done
 done
 done
