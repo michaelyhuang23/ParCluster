329986f606508f6a150772ec37bc93e4689ddc21
Michael Huang <michaelyhuang23@gmail.com>
2023-05-11 18:16:56 -0400

    alternate algo finished

--- include/parameter.h
+++ include/parameter.h
@@ -2,7 +2,7 @@
 #include "pargeo/point.h"
 #include "pargeo/ball.h"
 
-static const int dim = 2;
+static const int dim = 3;
 static double drange = 10;
 static double noiseCut = 0;
 static double depCut = 0;
@@ -11,4 +11,5 @@ static double depCut = 0;
 using point = pargeo::point<dim>;
 using ball = pargeo::_ball<dim, point>;
 using pointD = pargeo::pointD<dim, int>;
+using pointF = pargeo::pointD<dim, float>;
 
---
+++ tests/dpc_alternate.cpp
@@ -0,0 +1,107 @@
+#include<iostream>
+#include<fstream>
+#include<algorithm>
+#include<cstring>
+#include<unordered_set>
+
+#include "parlay/parallel.h"
+#include "parlay/sequence.h"
+#include "parlay/primitives.h"
+#include "pargeo/parseCommandLine.h"
+#include "pargeo/pointIO.h"
+#include "pargeo/point.h"
+#include "pargeo/getTime.h"
+#include "pargeo/unionFind.h"
+#include "priorityKdTree/psKdTree.h"
+#include "origKdTree/kdTree.h"
+
+#include "parameter.h"
+
+
+parlay::sequence<pointF> compute_densities(parlay::sequence<point>& ptrs, int K){
+	pargeo::origKdTree::node<dim, point>* tree = 
+		pargeo::origKdTree::build<dim, point>(ptrs, true, 1);
+	
+	parlay::sequence<pointF> ptrDs(ptrs.size());
+
+	size_t num_workers = parlay::num_workers();
+
+	auto knns = pargeo::origKdTree::batchKnn(ptrs, K, tree);
+
+	parlay::parallel_for(0, ptrs.size(), [&](size_t i){
+		float dist = ptrs[knns[i * K + K - 1]].dist(ptrs[i]);
+		ptrDs[i] = pointF(ptrs[i].coords(), 1/dist);
+	}, 1);
+
+	return ptrDs;
+}
+
+
+
+
+int main(int argc, char* argv[]) {
+	std::ios_base::sync_with_stdio(0);
+
+	pargeo::timer densityT, depT, linkageT;
+
+	pargeo::commandLine P(argc, argv, "-n{noiseCut} -d{depCut} -i{inFile} -k{densityK} -o{outFile}");
+	noiseCut = P.getOptionDoubleValue("-n", 0);
+	depCut = P.getOptionDoubleValue("-d", 0);
+	int K = P.getOptionIntValue("-k", 6);
+
+	char* iFile = P.getOptionValue("-i");
+	std::string oFile = P.getOptionValue("-o", std::string(""));
+	std::string dFile = P.getOptionValue("-decision", std::string(""));
+
+	parlay::sequence<point> ptrs = pargeo::pointIO::readPointsFromFile<point>(iFile);
+	int n = ptrs.size();
+	
+	densityT.start();
+	parlay::sequence<pointF> ptrDs = compute_densities(ptrs, K);
+	std::cout<<densityT.get_next()<<std::endl;
+
+	depT.start();
+	parlay::sequence<int> depPtr(n);
+	parlay::sequence<pointF> sptrs(ptrDs);
+	parlay::sequence<int> inverseMap(n);
+	parlay::parallel_for(0, n, [&inverseMap](int i){ inverseMap[i] = i; });
+	parlay::stable_sort_inplace(inverseMap, [&](int a, int b){ return pointF::attCompRev(sptrs[a], sptrs[b]); });
+	parlay::stable_sort_inplace(ptrDs, pointF::attCompRev);
+
+	pargeo::psKdTree::tree<dim, pointF>* root = pargeo::psKdTree::build<dim, pointF>(ptrDs, true, 16);
+	root->pargeo::psKdTree::node<dim, pointF>::initParallel();
+	parlay::parallel_for(0, n, [&](size_t i){
+		pointF* ptr = root->NearestNeighborBounded(i);
+		if(ptr)
+			depPtr[inverseMap[i]] = inverseMap[ptr->attribute];
+		else
+			depPtr[inverseMap[i]] = -1;
+	},1);
+	std::cout<<depT.get_next()<<std::endl;
+
+	if(dFile.size()>0){
+		std::ofstream fout(dFile);
+		for(int i=0;i<n;i++) fout<<sptrs[i].attribute<<" "<<sptrs[i].dist(sptrs[depPtr[i]])<<'\n';
+		fout.close();
+	}
+
+	linkageT.start();
+	pargeo::unionFind<int> UF(n);
+	parlay::parallel_for(0, n, [&](int i){
+		if(depPtr[i] != -1 && (sptrs[i].attribute < noiseCut || sptrs[i].distSqr(sptrs[depPtr[i]]) < depCut * depCut)){
+			UF.link(i, depPtr[i]);
+		}
+	});
+
+	parlay::sequence<int> cluster(n);
+	parlay::parallel_for(0, n, [&](int i){
+		cluster[i] = UF.find(i);
+	});
+	std::cout<<linkageT.get_next()<<std::endl;
+
+	if(oFile.size()>0){
+		std::ofstream fout(oFile);
+		for(int i=0;i<n;i++) fout<<cluster[i]<<'\n';
+		fout.close();
+	}
+}
